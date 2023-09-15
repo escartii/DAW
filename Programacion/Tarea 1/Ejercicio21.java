3b861bee6658446873c526781c40764e8de0c573
Alvaro Escarti <escartismx@gmail.com>
2023-09-15 23:56:43 +0200

    limpiando codigo

--- Programacion/Tarea 1/Ejercicio21.java
+++ Programacion/Tarea 1/Ejercicio21.java
@@ -3,30 +3,33 @@ public class Ejercicio21 {
     // variables enteras 
         String nombre = "Alvaro Escarti";
         System.out.println("Hecho por: "+nombre);
+        
+        
+        // Declaración de variabless
         int p = 200;
         int q = 201;
-        // \n sirve para hacer un salto de linea en la terminal para que se lea mejor
-        // uso el salto de linea para ahorrar lineas de codigo
-        System.out.println("Esto es la variable p: "+p+"\nEsto es la variable q: "+q);
         float x = 12.71f;
         float y = 12.72f;
         float z = 12.73f;
-        System.out.println("Float de X: "+x+"\nFloat de Y: "+y+"\nFloat de Z: "+z); 
         char a = 'a';
         char b = 'b';
         char c = 'c';
-        System.out.println("La variable char a vale: "+a+"\nLa variable char b vale: "+b+"\nLa variable char c vale: "+c);
         double raiz1 = 200.99;
         double raiz2 = 201.99;
-        System.out.println("La variable raiz1 vale: "+raiz1+"\nLa variable raiz2 vale: "+raiz2);
         long contador = 123456789883828382l;
-        System.out.println("La variable contador vale: "+contador);
         short indicador = 1000;
-        System.out.println("La variable indicador vale: "+indicador);
         byte valor = 22;
-        System.out.println("La variable byte valor vale: "+valor);
         boolean primero = true;
         boolean ultimo = false;
+        // \n sirve para hacer un salto de linea en la terminal para que se lea mejor
+        // uso el salto de linea para ahorrar lineas de codigo
+        System.out.println("Esto es la variable p: "+p+"\nEsto es la variable q: "+q);
+        System.out.println("Float de X: "+x+"\nFloat de Y: "+y+"\nFloat de Z: "+z); 
+        System.out.println("La variable char a vale: "+a+"\nLa variable char b vale: "+b+"\nLa variable char c vale: "+c);
+        System.out.println("La variable raiz1 vale: "+raiz1+"\nLa variable raiz2 vale: "+raiz2);
+        System.out.println("La variable contador vale: "+contador);
+        System.out.println("La variable indicador vale: "+indicador);
+        System.out.println("La variable byte valor vale: "+valor);
         System.out.println("La variable primero vale: "+primero+"\nLa variable ultimo vale: "+ultimo);
 
      }
