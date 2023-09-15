c642bfdb2576c0e8afdda4d22b1f3f5d075c1ea7
Alvaro Escarti <escartismx@gmail.com>
2023-09-16 00:04:49 +0200

    refactor code

--- Programacion/Tarea 1/Ejercicio31.java
+++ Programacion/Tarea 1/Ejercicio31.java
@@ -8,8 +8,17 @@ public class Ejercicio31 {
         int resultado = a+b+c;
         System.out.println("El resultado del ejercicio A es: "+resultado);
         ejercicioB(); // Restauro las variables a su valor original
+        ejercicioC();
+        ejercicioD();
+        ejercicioE();
+        ejercicioF();
+        ejercicioG();
+        ejercicioH();
+        ejercicioI();
+        ejercicioJ();
+        ejercicioK();
     }
-    public static void ejercicioB() {
+    public static void ejercicioB(){
         int a = 8; //Vuelvo a iniciar las variables a su valor para seguir operando
         int b = 3;
         int c = -5;
@@ -23,7 +32,6 @@ public class Ejercicio31 {
         int b = 3;
         int c = -5;
         System.out.println("El resultado del ejercicio C es: "+a/b);
-        ejercicioD();
     }
     public static void ejercicioD(){
         int a = 8;
@@ -31,7 +39,6 @@ public class Ejercicio31 {
         int c = -5;
         int resultado = a%b;
         System.out.println("El resultado del ejercicio D es: "+resultado);
-        ejercicioE();
     }
     public static void ejercicioE(){
         int a = 8;
@@ -39,7 +46,6 @@ public class Ejercicio31 {
         int c = -5;
         int resultado = a/c;
         System.out.println("El resultado del ejercicio E es: "+resultado);
-        ejercicioF();
     }
     public static void ejercicioF(){
         int a = 8;
@@ -47,7 +53,6 @@ public class Ejercicio31 {
         int c = -5;
         int resultado = a%c;
         System.out.println("El resultado del ejercicio F es: "+resultado);
-        ejercicioG();
     }
     public static void ejercicioG(){
         int a = 8;
@@ -55,7 +60,6 @@ public class Ejercicio31 {
         int c = -5;
         int resultado = a*b/c;
         System.out.println("El resultado del ejercicio G es: "+resultado);
-        ejercicioH();
     }
     public static void ejercicioH(){
         int a = 8;
@@ -63,7 +67,6 @@ public class Ejercicio31 {
         int c = -5;
         int resultado = a*(b/c);
         System.out.println("El resultado del ejercicio H es: "+resultado);
-        ejercicioI();
     }
     public static void ejercicioI(){
         int a = 8;
@@ -71,7 +74,6 @@ public class Ejercicio31 {
         int c = -5;
         int resultado = a*c%b;
         System.out.println("El resultado del ejercicio I es: "+resultado);
-        ejercicioJ();
     }
     public static void ejercicioJ(){
         int a = 8;
@@ -79,7 +81,6 @@ public class Ejercicio31 {
         int c = -5;
         int resultado = (a-3*b)%(c+2*a)/(a-c);
         System.out.println("El resultado del ejercicio J es: "+resultado);
-        ejercicioK();
     }
     public static void ejercicioK(){
         int a = 8;
