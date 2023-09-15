9ff02a0c7f491c19d39d33ff939c16f562f63a92
Alvaro Escarti <escartismx@gmail.com>
2023-09-16 00:02:27 +0200

    refactor code

--- Programacion/Tarea 1/Ejercicio22.java
+++ Programacion/Tarea 1/Ejercicio22.java
@@ -1,45 +1,41 @@
 public class Ejercicio22 {
     public static void main (String[] args) {
-        int edad = 22; 
-        System.out.println(edad);
+       
+       // Declaracion de variables
+        int edad = 22;
         int codigoPostal = 46240;
-        System.out.println(codigoPostal);
-        double altura = 1.70; 
-        System.out.println(altura);
+        double altura = 1.70;
         double miPeso = 72.5;
-        System.out.println(miPeso);
         char hombre = 'H';
-        System.out.println(hombre);
         char mujer = 'M';
-        System.out.println(mujer);
-        
         String nombre = "Alvaro";
-        System.out.println(nombre);
-        byte numHijos = 11; // Utilizo byte porque nadie tiene mas de 127 hijos.
-        System.out.println(numHijos);
-        int porcentajeIVA = 21;
-        System.out.println(porcentajeIVA+" %");
+        int numHijos = 11;
+        int porcentajeIVA = 21; 
         double precio = 79.99;
-        System.out.println(precio+"€");
-
         String mensaje = "Hola buenas tardes, bienvenido a mi segundo ejercicio de java";
-
-        System.out.println(mensaje);
         int minutos = 60;
-        System.out.println(minutos);
-        int dias = 30;
-        System.out.println(dias);
-
-        String matricula = "5473CXJ";
-
-        System.out.println(matricula);
+        int dias = 30;     
+        String matricula = "5473CXJ";      
         int contador = 0;
-        System.out.println(contador);
         boolean mayorEdad = true;
-        System.out.println(mayorEdad);
-
         String tallaCamiseta = "S,M,L,XL,XXL";
-
+       
+        System.out.println(edad);
+        System.out.println(codigoPostal); 
+        System.out.println(altura);
+        System.out.println(miPeso);
+        System.out.println(hombre);
+        System.out.println(mujer);
+        System.out.println(nombre);
+        System.out.println(numHijos);
+        System.out.println(porcentajeIVA+" %");
+        System.out.println(precio+"€");
+        System.out.println(mensaje);
+        System.out.println(minutos);
+        System.out.println(dias);
+        System.out.println(matricula);
+        System.out.println(contador);
+        System.out.println(mayorEdad);
         System.out.println(tallaCamiseta);
     }
 }
