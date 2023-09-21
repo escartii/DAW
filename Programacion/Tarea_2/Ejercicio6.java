f7967beda9a33de0ed2e57adeb326551444c8d7f
Alvaro Escarti <escartismx@gmail.com>
2023-09-21 11:54:59 +0200

    ejercicios java

---
+++ Programacion/Tarea_2/Ejercicio6.java
@@ -0,0 +1,17 @@
+public class Ejercicio6 {
+    public static void main (String[] args){
+        String s1="Alvaro Escarti Lamolda";  
+        int index1=s1.indexOf(" "); // Buscamos el primer espacio 
+        int index2=s1.indexOf(" ", index1 +1); // 
+        String nombre = s1.substring(0,index1); // recorre desde el caracter 0 hasta index 1 que es 6
+        String apellido1 = s1.substring(index1 +1,index2); // recorre desde el caracter 6 + 1, que es el espacio hasta el sigueinte espacio
+        String apellido2 = s1.substring(index2 +1); // 
+        System.out.println(nombre);
+        System.out.println(apellido1);
+        System.out.println(apellido2);
+
+        System.out.println("Mi nombre tiene:  " + nombre.length() + " caracteres");
+        System.out.println("Mi primer apellido tiene: " + apellido1.length() + " caracteres");
+        System.out.println("Mi segundo apellido tiene:  " + apellido2.length() + " caracteres");
+        }
+    }
---
+++ Sistemas/test.py
@@ -0,0 +1,5 @@
+import random
+import string
+# Generar una letra aleatoria entre "a" y "z"
+string.ascii_letters = 'abcdefghijklmnopqrstuvwxyz'
+print (random.choice(string.ascii_letters))
