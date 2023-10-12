ef3bdb5f30d27b8359bd3475f9783aefe9b6a5f0
Alvaro Escarti <escartismx@gmail.com>
2023-10-12 20:31:10 +0200

    añadiendo tareas

---
+++ Programacion/test.java
@@ -0,0 +1,12 @@
+
+public class test {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 100; i++) {
+            if (i % 5 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
+       
+ 
