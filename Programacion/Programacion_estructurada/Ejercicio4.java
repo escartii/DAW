5c2c52789837343cdf4efd0f9f598ecaa051d057
Alvaro Escarti <escartismx@gmail.com>
2023-10-17 17:47:52 +0200

    ejercicios java

--- Programacion/Programacion_estructurada/Ejercicio4.java
+++ Programacion/Programacion_estructurada/Ejercicio4.java
@@ -1,5 +1,25 @@
-public class Ejercicio4 {
-    public static void main(String[] args) {
-       
+
+    import java.util.Scanner;
+
+    public class Ejercicio4 {
+        public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = input.nextInt();
+        boolean isPrime = true;
+        int i = 2;
+        while (i <= num / 2) {
+            if (num % i == 0) {
+                isPrime = false;
+                break;
+            }
+            i++;
+        }
+        if (isPrime) {
+            System.out.println(num + " is a prime number.");
+        } else {
+            System.out.println(num + " is not a prime number.");
+         }
     }
 }
+
---
+++ Programacion/Programacion_estructurada/Ejercicio7.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class Ejercicio7 {
+    public static void main(String[] args) {
+
+        // Declaro las variables
+        int numUsuario;
+        int numeroAleatorio;
+
+        Scanner sc = new Scanner(System.in);
+        // Compruebo que el dia introducido es correcto
+        do {
+            System.out.println("Opcion 1) Piedra");
+            System.out.println("Opcion 2) Papel");
+            System.out.println("Opcion 3) Tijera");
+            System.out.print("Elige una opción: ");
+            numUsuario = sc.nextInt();
+        } while (numUsuario <= 0 || numUsuario >= 4);
+        // Numero random que juega el PC
+        numeroAleatorio = (int) (Math.random()*3+1);
+
+        if (numUsuario == numeroAleatorio) {
+            System.out.println("Empate");
+        } else if (numUsuario == 1 && numeroAleatorio == 2) {
+            System.out.println("Has elegido: Piedra" + "\nEl PC ha elegido: Papel");
+            System.out.println("Gana el PC");
+        } else if (numUsuario == 1 && numeroAleatorio == 3) {
+            System.out.println("Has elegido Piedra" + "\nEl PC ha elegido: Tijera");
+            System.out.println("Gana el usuario");
+        } else if (numUsuario == 2 && numeroAleatorio == 1) {
+            System.out.println("Has elegido Papel" + "\nEl PC ha elegido: Piedra");
+            System.out.println("Gana el usuario");
+        } else if (numUsuario == 2 && numeroAleatorio == 3) {
+            System.out.println("Has elegido Papel" + "\nEl PC ha elegido: Tijera");
+            System.out.println("Gana el PC");
+        } else if (numUsuario == 3 && numeroAleatorio == 1) {
+            System.out.println("Has elegido Tijera" + "\nEl PC ha elegido: Piedra");
+            System.out.println("Gana el PC");
+        } else if (numUsuario == 3 && numeroAleatorio == 2) {
+            System.out.println("Has elegido Tijera" + "\nEl PC ha elegido: Papel");
+            System.out.println("Gana el usuario");
+        }
+    }
+}
+
+
