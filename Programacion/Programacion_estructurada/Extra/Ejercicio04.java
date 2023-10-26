c94ca59024d609aa202f6b3eab58f5ee7946410a
Alvaro Escarti <escartismx@gmail.com>
2023-10-26 23:36:55 +0200

    ejercicio 4 extra

--- Programacion/Programacion_estructurada/Extra/Ejercicio04.java
+++ Programacion/Programacion_estructurada/Extra/Ejercicio04.java
@@ -5,14 +5,15 @@ import java.util.Scanner;
 public class Ejercicio04 {
     public static void main(String[] args) {
         
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Introduce un número: ");
-        int columnas = sc.nextInt();
-         
+        int columnas;
         int k = 0;
         int contador = 0;
         int contador2 = 0;
 
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Introduce un número: ");
+        columnas = sc.nextInt();
+         
         for (int i = 1; i <= columnas; ++i) {
           for (int space = 1; space <= columnas - i; ++space) {
             System.out.print("  ");
@@ -27,11 +28,9 @@ public class Ejercicio04 {
               ++contador2;
               System.out.print((i + k - 2 * contador2) + " ");
             }
-    
             ++k;
           }
           contador2 = contador = k = 0;
-    
           System.out.println();
         }
     }
