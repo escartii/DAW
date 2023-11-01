f9a327daee12eec47ceaae8468d78f8a3e6b180a
Alvaro Escarti <escartismx@gmail.com>
2023-11-01 19:19:25 +0100

    ejercicio calculadora

--- Programacion/Programacion_estructurada/Extra/CalculadoraRPN.java
+++ Programacion/Programacion_estructurada/Extra/CalculadoraRPN.java
@@ -1,20 +1,22 @@
 package Programacion_estructurada.Extra;
-import java.util.Scanner;
 
+import java.util.Scanner;
 
 public class CalculadoraRPN {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+
         double[] registros = new double[4];
         int indice = 0;
         boolean salir = false;
-
+        Scanner sc = new Scanner(System.in);
+        // Mientras no se introduzca Q (MAYUSCULA) el programa sigue pidiendo números
         while (!salir) {
             System.out.print("Introduce un número o una operación (+, -, *, /) o Q (MAYUSCULA) para salir: ");
             String entrada = sc.nextLine();
-
+            // Si se introduce un número, se guarda en el array y se incrementa el índice
             if (entrada.equals("+") && indice < 2) {
                 System.out.println("Lo siento, no hay suficientes números en la fila para hacer calculos");
+                // Si se introduce un + y hay al menos dos números en el array, se suman los dos últimos números
             } else if (entrada.equals("+")) {
                 double suma = registros[indice - 2] + registros[indice - 1];
                 registros[indice - 2] = suma;
@@ -40,22 +42,18 @@ public class CalculadoraRPN {
             } else if (entrada.equals("Q")) {
                 salir = true;
             } else {
-                try {
-                    double numero = Double.parseDouble(entrada);
-                    registros[indice] = numero;
-                    indice++;
-                } catch (NumberFormatException e) {
-                    System.out.println("Entrada no válida");
-                }
+                // Si se introduce un número, se guarda en el array y se incrementa el índice
+                double numero = Double.parseDouble(entrada);
+                registros[indice] = numero;
+                indice++;
             }
-
+            // Se muestran los números que hay en el array
             System.out.print("Registros de la fila: ");
             for (int i = 0; i < indice; i++) {
                 System.out.print(registros[i] + " ");
             }
             System.out.println();
         }
-
         System.out.println("Saliendo del programa.");
     }
 }
