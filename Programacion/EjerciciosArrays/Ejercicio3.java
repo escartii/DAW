55d2af65439e508b9a39459de86878b12867b13e
Alvaro Escarti <escartismx@gmail.com>
2023-11-02 18:46:05 +0100

    ejercicio java

--- Programacion/EjerciciosArrays/Ejercicio3.java
+++ Programacion/EjerciciosArrays/Ejercicio3.java
@@ -11,34 +11,33 @@ public class Ejercicio3 {
         Scanner sc = new Scanner(System.in);
         // Rellenar la matriz por teclado
 
-            while (i < 4) {
-                int j = 0;
-                while (j < 4) {
-                    System.out.print("Introduce el valor de la posición [" + i + "][" + j + "]: ");
-                    m1[i][j] = sc.nextInt();
-                    j++;
-                }
-                i++;
+        while (i < 4) {
+            int j = 0;
+            while (j < 4) {
+                System.out.print("Introduce el valor de la posición [" + i + "][" + j + "]: ");
+                m1[i][j] = sc.nextInt();
+                j++;
             }
-
+            i++;
+        }
+        // reutilizo i
+        i=0;
         // Comprobar si la matriz es simétrica
-            while (i < 4) {
-                for (int j = 0; j < 4; j++) {
-                    if (m1[i][j] != m1[j][i]) {
-                        esSimetrica = false;
-                        break;
-                    }
+        while (i < 4) {
+            for (int j = 0; j < 4; j++) {
+                if (m1[i][j] != m1[j][i]) {
+                    esSimetrica = false;
+                    break;
                 }
-                i++;
             }
-
+            i++;
+        }
         // Mostrar el resultado
         if (esSimetrica) {
             System.out.println("La matriz es simétrica");
         } else {
             System.out.println("La matriz no es simétrica");
         }
-
         // muestrame la matriz por pantalla
         System.out.println("La matriz es: ");
         while (c < 4) {
