a48f5c887f3577a0f5744b06d10729bc837c9fb9
Alvaro Escarti <escartismx@gmail.com>
2023-11-03 18:32:10 +0100

    examen

--- Programacion/PreparacionExamen/PreparacionExamen.java
+++ Programacion/PreparacionExamen/PreparacionExamen.java
@@ -2,11 +2,9 @@ package PreparacionExamen;
 
 public class PreparacionExamen {
     public static void main(String[] args) {
-    boolean hayGanador = false;
-    // cambio de int a double
-    int valorEntero = 5;
-    double doubleValue = valorEntero;
-
+        boolean hayGanador = false;
+        // cambio de int a double
+        int valorEntero = 5;
         if (hayGanador) {
             // cuando se cumple la condicion, salimos
             hayGanador = true;
@@ -15,45 +13,74 @@ public class PreparacionExamen {
             hayGanador = false;
         }
 
-          // String a substring
-          String str = "¡Hola, Mundo!";
-          String subStr = str.substring(7);
-  
-          // Declaración de condicional if
-          if (valorEntero < 10) {
-              System.out.println("El valor es menor que 10.");
-          } else {
-              System.out.println("El valor no es menor que 10.");
-          }
-  
-          // Bucle while
-          int i = 0;
-          while (i < 5) {
-              System.out.println("Iteración de bucle while: " + i);
-              i++;
-          }
-  
-          // Bucle for
-          for (int j = 0; j < 5; j++) {
-              System.out.println("Iteración de bucle for: " + j);
-          }
-  
-          // Arreglos
-          int[] arregloEnteros = {1, 2, 3, 4, 5};
-          System.out.println("Elementos del arreglo:");
-          for (int k = 0; k < arregloEnteros.length; k++) {
-              System.out.println("Elemento en el índice " + k + ": " + arregloEnteros[k]);
-          }
-  
-          // Matrices (arreglos 2D)
-          int[][] matriz = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-          System.out.println("Elementos de la matriz:");
-          for (int f = 0; f < matriz.length; f++) {
-              for (int c = 0; c < matriz[f].length; c++) {
-                  System.out.print(matriz[f][c] + " ");
-                }
-              System.out.println();
-          }
-    }
-}
+        // String a substring
+        String str = "¡Hola, Mundo!";
+        // Declaración de condicional if
 
+        System.out.println(" ");
+        System.out.println("*--SENTENCIA IF--*");
+        System.out.println(" ");
+
+        if (valorEntero < 10) {
+            System.out.println("El valor es menor que 10.");
+        } else {
+            System.out.println("El valor es mayor que 10.");
+        }
+
+        System.out.println(" ");
+        System.out.println("*--BUCLE WHILE--*");
+        System.out.println(" ");
+        // Bucle while
+        int i = 0;
+        while (i < 5) {
+            System.out.println("Iteración de bucle while: " + i);
+            i++;
+        }
+
+        System.out.println(" ");
+        System.out.println("*--BUCLE FOR--*");
+        System.out.println(" ");
+        // Bucle for
+        for (int j = 0; j < 5; j++) {
+            System.out.println("Iteración de bucle for: " + j);
+        }
+
+        System.out.println(" ");
+        System.out.println("*--ARRAYS O VECTORES--*");
+        System.out.println(" ");
+
+        // Arreglos
+        int[] arregloEnteros = { 1, 2, 3, 4, 5 };
+        System.out.println("Elementos del array:");
+        for (int k = 0; k < arregloEnteros.length; k++) {
+            System.out.println("Elemento en el índice " + k + ": " + arregloEnteros[k]);
+        }
+
+        System.out.println(" ");
+        System.out.println("*--MATRICES O ARRAYS 2D--*");
+        System.out.println(" ");
+
+        // Matrices (arreglos 2D)
+       
+         // Comprobar si dos matrices son iguales
+            int[][] matriz1 = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+            int[][] matriz2 = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+            boolean iguales = true;
+
+            for (int f = 0; f < matriz1.length; f++) {
+                for (int c = 0; c < matriz1[f].length; c++) {
+                    if (matriz1[f][c] != matriz2[f][c]) {
+                        iguales = false;
+                    }
+                }
+            }
+            System.out.println(matriz1[1][1]);
+            System.out.println(matriz1[1][1] * 5);
+
+            if (iguales) {
+                System.out.println("Las matrices son iguales");
+            } else {
+                System.out.println("Las matrices no son iguales");
+            }
+        }
+    }
