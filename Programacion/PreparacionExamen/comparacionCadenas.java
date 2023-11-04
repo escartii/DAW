e0ecaf342d169f9a5e90e81a96411428cff698c7
Alvaro Escarti <escartismx@gmail.com>
2023-11-04 22:45:18 +0100

    cadenas de texto

---
+++ Programacion/PreparacionExamen/comparacionCadenas.java
@@ -0,0 +1,16 @@
+package PreparacionExamen;
+
+public class comparacionCadenas {
+    public static void main(String[] args) {
+
+        String[] nombres = { "Juan", "María", "Pedro", "Ana", "Luis" };
+        
+
+        for (int i = 0; i < nombres.length; i++){
+            // Mostrar todo el array cada nombre en una linea
+            System.out.println(nombres[i]);
+            // Mostrar todo el array en una linea
+            System.out.print(nombres[i] + " ");
+        }
+    }
+}
