4c28d1e5f704519c31165c9a8c30815ab332c697
Alvaro Escarti <escartismx@gmail.com>
2023-11-04 20:46:25 +0100

    ejercicio numeros iguales en un array

---
+++ Programacion/PreparacionExamen/Ejercicioarreglos.java
@@ -0,0 +1,34 @@
+package PreparacionExamen;
+
+public class Ejercicioarreglos {
+    public static void main(String[] args) {
+        // Generar un arreglo con numero aleatorios
+        int numRandom;
+        numRandom = (int) (Math.random() * 5);
+        int[] miVector = new int[3];
+        boolean salir = false;
+
+        for (int i = 0; i < miVector.length; i++) {
+            numRandom = (int) (Math.random() * 50);
+            System.out.println(numRandom);
+            miVector[i] = numRandom;
+
+            for (int j = 0; j < i; j++) {
+                if (miVector[i] == miVector[j]) {
+                    salir = true;
+                }
+            }
+
+            if (salir) {
+                System.out.println("Hay un numero repetido.");
+                salir = true;
+            }
+        }
+
+        if (!salir) {
+            System.out.println("No hay numeros repetidos.");
+        }
+    }
+    // System.out.println(miVector[0]);
+
+}
