4f2b2e192995357f2e44a613ab2db9a34788dada
Alvaro Escarti <escartismx@gmail.com>
2023-11-14 20:34:38 +0100

    primera funcion sumar java

---
+++ Programacion/Funciones/primerafuncion.java
@@ -0,0 +1,17 @@
+package Funciones;
+
+public class primerafuncion {
+
+    static int sumar (int a, int b) {
+        int resultado;
+        resultado = a + b;
+        return resultado;
+    }
+    public static void main(String[] args) {
+        int num1 = 3;
+        int num2 = 5;
+        int suma = 0;
+        suma = sumar(num1,num2);
+        System.out.println("La suma de " + num1 + " Y " + num2 + " es " + suma);
+    }
+}
