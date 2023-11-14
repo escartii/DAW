ac275692677bd9fdc805273233590f11f5cd4e2c
Alvaro Escarti <escartismx@gmail.com>
2023-11-14 22:10:25 +0100

    a

--- Programacion/Examen/ExamenDAM/ejercicio1.java
+++ Programacion/Examen/ExamenDAM/ejercicio1.java
@@ -1,23 +1,47 @@
 package Examen.ExamenDAM;
 
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class ejercicio1 {
+
     public static void main(String[] args) {
-        
         Scanner in = new Scanner(System.in);
         int num;
+        System.out.print("Cuantas comprobaciones quieres hacer: ");
         num = in.nextInt();
-        int matriz[][] = new int[num][2];
 
-        for (int i = 0; i < num; i++) {
-            matriz[i][0] = in.nextInt();
-            matriz[i][1] = in.nextInt();
+        for (int k = 0; k < num; k++) {
+            int numClientes = in.nextInt();
+            int[][] maletas = new int[numClientes][];
+            for (int i = 0; i < numClientes; i++) {
+                int numMaletas = in.nextInt();
+                int[] maletasCliente = new int[numMaletas];
+                for (int j = 0; j < numMaletas; j++) {
+                    maletasCliente[j] = in.nextInt();
+                }
+                Arrays.sort(maletasCliente);
+                maletas[i] = maletasCliente;
+            }
+
+            int numViajes = 0;
+            int[] car = new int[10];
+            int index = 0;
+            for (int i = 0; i < numClientes; i++) {
+                for (int j = 0; j < maletas[i].length; j++) {
+                    if (index == 10) {
+                        numViajes++;
+                        car = new int[10];
+                        index = 0;
+                    }
+                    car[index] = maletas[i][j];
+                    index++;
+                }
+            }
+            if (index > 0) {
+                numViajes++;
+            }
+            System.out.println(numViajes);
         }
-
-        for (int i = 0; i < num; i++) {
-            System.out.println(matriz[i][0] + matriz[i][1]);
-        }
-
     }
 }
