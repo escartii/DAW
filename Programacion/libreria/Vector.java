22ce1c63689dff5f1153597abef9663af277e5b2
Alvaro <escartismx@gmail.com>
2023-11-16 15:43:07 +0100

    añado librerias

--- Programacion/libreria/Vector.java
+++ Programacion/libreria/Vector.java
@@ -18,4 +18,17 @@ public class Vector {
     public static String devuelveValoStr(int pos, String[] v) {
         return v[pos];
     }
+
+    public static boolean esIgualStr(String[]a, String[] b) {
+        boolean resultado = true;
+        if (a.length == b.length){
+            for (int i = 0; i < a.length; i++){
+                if (a[i] != b[i]){
+                    resultado = false;
+                }
+            }
+            return resultado;
+        }
+        return false;
+    }
 }
