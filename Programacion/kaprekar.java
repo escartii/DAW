a8480339738605a9b59258a963939ec977174cda
Alvaro Escarti <escartismx@gmail.com>
2023-11-17 12:53:16 +0100

    kaprekar

---
+++ Programacion/kaprekar.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class kaprekar {
+    public static void main(String[] args) {
+        // Alvaro Escartí 1º DAW 
+        int cuadrado;
+        int num;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Escribe un numero: ");
+        num = sc.nextInt();
+
+        cuadrado = num * num;
+        String numeros = String.valueOf(cuadrado);
+        char[] digitos = numeros.toCharArray();
+        System.out.println("\nEl numero que has introducido elevado a 2, es: " + cuadrado);
+
+        int suma = 0;
+        sc.close();
+
+        String cuadradoStr = String.valueOf(cuadrado);
+        for (int i = 0; i < cuadradoStr.length(); i++) {
+            char numSeparado = digitos[i];
+            int cuadradoNum = Character.getNumericValue(numSeparado);
+            suma = suma + cuadradoNum;
+        }
+        System.out.println("La suma de los números es: " + suma);
+
+        if (suma == num) {
+            System.out.println("El numero es Kaprekar");
+        } else {
+            System.out.println("El numero no es Kaprekar");
+        }
+    }
+}
