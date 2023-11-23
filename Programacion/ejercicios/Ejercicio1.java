81b9cf9e5dc1cffc528c8d89c4fed66c4e609dda
Alvaro <escartismx@gmail.com>
2023-11-23 15:53:10 +0100

    ejercicios java

--- Programacion/ejercicios/Ejercicio1.java
+++ Programacion/ejercicios/Ejercicio1.java
@@ -25,42 +25,5 @@ public class Ejercicio1 {
         }
         System.out.println(" ");
         System.out.println("Hay: " + contador + " numeros primos");
-
-        // Ejercicio 2
-        System.out.println(" ");
-        System.out.println("Ejercicio 2");
-        for (int i = 1000; i <= 9999; i++) {
-            esCapicua = FNumDaw.esCapicua(i);
-            if (esCapicua) {
-                System.out.println("El numero: " + i + " Es capicua");
-                contadorCapi++;
-            }
-        }
-        System.out.println(" ");
-        System.out.println("Hay: " + contadorCapi + " numeros capicua");
-
-        // Acepta el reto 210
-        
-        int n;
-        n = sc.nextInt();
-        while (n != 0) {
-            // Declaramos las variables necesarias
-            String palabra = sc.next();
-            int suma = 0;
-
-            // Calculamos la suma de los códigos ASCII de los caracteres de la palabra y su largaria
-            for (int i = 0; i < palabra.length(); i++) {
-                suma += palabra.charAt(i);
-            }
-
-            // Obtenemos el número primo inferior más cercano a la suma
-            int primo = FNumDaw.anteriorPrimo(suma);
-
-            // Imprimimos la respuesta
-            System.out.println(primo);
-            n--;
-        }
-
-        // Acepta el reto 1
     }
 }
