026b52d8174ff6987eebbe42a648442bb74ab1f6
Alvaro <escartismx@gmail.com>
2023-11-30 16:07:02 +0100

    calcualdora

--- Programacion/calculadoraRPN/calculadora.java
+++ Programacion/calculadoraRPN/calculadora.java
@@ -2,62 +2,78 @@ package calculadoraRPN;
 
 import java.util.Scanner;
 
-
-
 public class calculadora {
-    
-    public static int[] volteaArrayInt(int[] array) {
-        int[] arrayVolteado = new int[array.length];
+    public static void main(String[] args) {
+
+        boolean salir = false;
+        String num = "";
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Escribe un numero entre 4 y 20: ");
+        num = sc.nextLine();
+
+        int size = Integer.parseInt(num);
+        double[] registros = new double[size];
+
+        while (!salir) {
+            int numrev = 0;
+            double[] volt = volteaArrayInt(registros);
+            numrev = registros.length;
+            for (int i = 0; i < registros.length; i++) {
+                System.out.println(numrev + ": " + volt[i]);
+                numrev = numrev -1;
+            }
+            System.out.print("Introduce un numero o un caracter de operacion (+, -, *, /): ");
+            num = sc.nextLine();
+
+            if (num.equalsIgnoreCase("Q")) {
+                System.out.println("Adios");
+                salir = true;
+            } else if (num.equals("+") || num.equals("-") || num.equals("*") || num.equals("/")) {
+                if (num.equals("+")) {
+                    double temp = registros[1] + registros[0];
+                    for (int i = 1; i <= registros.length - 1; i++) {
+                        registros[i-1] = registros[i];
+                    }
+                    registros[0] = temp;   
+                }
+                if (num.equals("-")) {
+                    double temp = registros[1] - registros[0];
+                    for (int i = 1; i <= registros.length - 1; i++) {
+                        registros[i-1] = registros[i];
+                    }
+                    registros[0] = temp;   
+                }
+                if (num.equals("*")) {
+                    double temp = registros[1] * registros[0];
+                    for (int i = 1; i <= registros.length - 1; i++) {
+                        registros[i-1] = registros[i];
+                    }
+                    registros[0] = temp;   
+                }
+                if (num.equals("/")) {
+                    double temp = registros[1] / registros[0];
+                    for (int i = 1; i <= registros.length - 1; i++) {
+                        registros[i-1] = registros[i];
+                    }
+                    registros[0] = temp;   
+                }
+                // ...
+            } else {
+                double numDouble = Double.parseDouble(num);
+                //double ultimoNumero = registros[registros.length - 1];
+                for (int i = registros.length - 1; i > 0; i--) {
+                    registros[i] = registros[i - 1];
+                }
+                registros[0] = numDouble;
+            }
+        }
+    }
+
+    public static double[] volteaArrayInt(double[] array) {
+        double[] arrayVolteado = new double[array.length];
         for (int i = 0; i < array.length; i++) {
             arrayVolteado[i] = array[array.length - 1 - i];
         }
         return arrayVolteado;
     }
-
-    public static void main(String[] args) {
-        
-        boolean salir = false;
-        int num = 0;
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Escribe un numero entre 4 y 20: ");
-        num = sc.nextInt();
-
-        if (num < 4 || num > 20) {
-            System.out.println("El número debe estar entre 4 y 20. Inténtalo de nuevo.");
-            return;
-        }
-
-        double[] registros = new double[num];
-
-        for (int i = 0; i < registros.length; i++) {
-            System.out.println(i+1 + ": " + registros[i]);
-        }
-
-        while (!salir) {
-            for (int i = 0; i < registros.length; i++) {
-                System.out.println("Introduce un numero (presiona 'q' para salir): ");
-                String input = sc.next();
-                if (input.equals("q")) {
-                    salir = true;
-                }
-                num = Integer.parseInt(input);
-                registros[i] = num;
-                System.out.println(registros[i]);
-            }
-
-            int[] intRegistros = new int[registros.length];
-            for (int i = 0; i < registros.length; i++) {
-                intRegistros[i] = (int) registros[i];
-            }
-
-            intRegistros = volteaArrayInt(intRegistros);
-            int bajarNum = intRegistros.length;
-            for (int i = 0; i < intRegistros.length; i++) {
-                System.out.println((bajarNum) + ": " + intRegistros[i]);
-                bajarNum-=1;
-            }
-        }
-        
-    }
 }
-
