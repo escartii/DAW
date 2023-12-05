0fde4a529c7e53af361f3ad18b45b01981cf910e
Alvaro Escarti <escartismx@gmail.com>
2023-12-05 12:19:26 +0100

    funcion

--- Programacion/Funciones/primerafuncion.java
+++ Programacion/Funciones/primerafuncion.java
@@ -7,11 +7,20 @@ public class primerafuncion {
         resultado = a + b;
         return resultado;
     }
+
+    public static String saludar(String nombre) {
+        String saludo = "Hola " + nombre;
+        return saludo;
+    }
+
     public static void main(String[] args) {
         int num1 = 3;
         int num2 = 5;
         int suma = 0;
-        suma = sumar(num1,num2);
+        suma = sumar(num1, num2);
         System.out.println("La suma de " + num1 + " + " + num2 + " es = " + suma);
+
+        String saludo = saludar("Alvaro");
+        System.out.println(saludo);
     }
 }
