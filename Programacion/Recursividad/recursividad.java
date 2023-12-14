c3257ec69f3bba81bdc09407f75a8585580ed5b7
Alvaro Escarti <escartismx@gmail.com>
2023-12-14 14:59:19 +0100

    recursividad

---
+++ Programacion/Recursividad/recursividad.java
@@ -0,0 +1,24 @@
+package Recursividad;
+
+public class recursividad {
+
+    public static int fibonacci(int N) {
+        int res = 0;
+        if (N == 0) {
+            res = 0;
+        } else if (N == 1) {
+            res = 1;
+        } else {
+            res = fibonacci(N - 1) + fibonacci(N - 2);
+        }
+        return res;
+    }
+    public static void main(String[] args) {
+
+        // Cojo el primer argumento y lo convierto a entero 
+        System.out.println(fibonacci(Integer.valueOf(args[0])));
+        System.out.println(fibonacci(Integer.valueOf(args[1])));
+        System.out.println(fibonacci(Integer.valueOf(args[2])));
+    }
+}
+
