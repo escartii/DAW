a4fde68cdb8161b1753186547ed578c630dc99b0
Alvaro Escarti <escartismx@gmail.com>
2024-01-09 20:01:17 +0100

    Modificando script

--- Sistemas/commit.sh
+++ Sistemas/commit.sh
@@ -2,7 +2,7 @@
 usuario=Escartí
 echo "Hecho por: "$usuario
 
-echo hola mundo > holamundo.sh
+echo "hola mundo" >> Commitdiario.txt
 git add .
 git commit -m "Modificando script"
 git push origin main
