6676abfe95fa87899b240f5f4165ed7fc56a1147
Alvaro Escarti <escartismx@gmail.com>
2024-01-12 18:24:13 +0100

    fixed

--- Programacion/Programacion_Dinamica/calculadoraRPN.java
+++ Programacion/Programacion_Dinamica/calculadoraRPN.java
@@ -1,7 +1,25 @@
 package Programacion_Dinamica;
-
+import java.util.Scanner;
 public class calculadoraRPN {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        int respuesta = sc.nextInt();
+
+
+        switch (respuesta) {
+            case 1:
+                System.out.println("Has elegido sumar: ");
+                break;
+            case 2:
+                System.out.println("Has elegido restar:");
+            default:
+                break;
+        }
+
+
+
+
+
+
     }
 }
