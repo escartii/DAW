af6bbd3311152a2dd8daca2a577d0221e623f5cb
Alvaro Escarti <escartismx@gmail.com>
2024-01-16 19:20:22 +0100

    ejercicio estadistica, media y desviacion tipica

--- Programacion/Programacion_Dinamica/Estadistica.java
+++ Programacion/Programacion_Dinamica/Estadistica.java
@@ -3,29 +3,35 @@ package Programacion_Dinamica;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+    public class Estadistica {
+        public static void main(String[] args) {
+            List<Float> numeros = new ArrayList<>();
+            boolean salir = false;
+            float sumaNumeros = 0;
+            float sumaCuadrados = 0;
+            Scanner scanner = new Scanner(System.in);
 
-public class Estadistica {
-    public static void main(String[] args) {
-        List<Float> numeros = new ArrayList<>();
-        float sumaNumeros = 0;
-        float sumaCuadrados = 0;
-        Scanner scanner = new Scanner(System.in);
-
-        while (true) {
-            System.out.print("Ingrese un número (o 'q' para salir): ");
-            String input = scanner.nextLine();
-
-            if (input.equalsIgnoreCase("q")) {
-                System.exit(0);
+            while (!salir) {
+                System.out.print("Ingrese un número (o 'q' para salir): ");
+                String input = scanner.nextLine();
+                // Si pulsamos q salimos del programa
+                if (input.equalsIgnoreCase("q")) {
+                    salir = true;
+                // Si no, seguimos dentro del bucle
+                } else {
+                    float numero = Float.parseFloat(input);
+                    numeros.add(numero);
+                    sumaNumeros += numero;
+                    sumaCuadrados += numero * numero;
+                    float media = sumaNumeros / numeros.size();
+                    float desviacion = (float) Math.sqrt((sumaCuadrados / numeros.size()) - (media * media));
+                    System.out.print("\u001B[32m"); // Cambiar color a verde
+                    System.out.print("Media estadística: " + media);
+                    System.out.print("     ");
+                    System.out.println("Desviación típica: " + desviacion);
+                    System.out.print("\u001B[0m"); // Restaurar color original
+                }
             }
-            float numero = Float.parseFloat(input);
-            numeros.add(numero);
-            sumaNumeros += numero;
-            sumaCuadrados += numero * numero;
-            float media = sumaNumeros / numeros.size();
-            float desviacion = (float) Math.sqrt((sumaCuadrados / numeros.size()) - (media * media));
-            System.out.print("Media estadística: " + media);
-            System.out.println("     " + "Desviación típica: " + desviacion);
         }
     }
-}
+
