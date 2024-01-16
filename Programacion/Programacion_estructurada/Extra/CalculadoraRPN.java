844f6007e8126c6574528aa8c7aac359aa3d2808
Alvaro Escarti <escartismx@gmail.com>
2024-01-16 18:18:45 +0100

    calculadora rpn con programacion dinamica

--- Programacion/Programacion_estructurada/Extra/CalculadoraRPN.java
+++ Programacion/Programacion_estructurada/Extra/CalculadoraRPN.java
@@ -1,55 +1,57 @@
 package Programacion_estructurada.Extra;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class CalculadoraRPN {
     public static void main(String[] args){
 
-        double[] registros = new double[4];
-        int indice = 0;
+        Stack<Double> registros = new Stack<>();
         boolean salir = false;
         Scanner sc = new Scanner(System.in);
         // Mientras no se introduzca Q (MAYUSCULA) el programa sigue pidiendo números
         while (!salir) {
             System.out.print("Introduce un número o una operación (+, -, *, /) o Q (MAYUSCULA) para salir: ");
             String entrada = sc.nextLine();
-            // Si se introduce un número, se guarda en el array y se incrementa el índice
-            if (entrada.equals("+") && indice < 2) {
-                System.out.println("Lo siento, no hay suficientes números en la fila para hacer calculos");
-                // Si se introduce un + y hay al menos dos números en el array, se suman los dos últimos números
+            // Si se introduce un número, se guarda en el stack
+            if (entrada.equals("+") && registros.size() < 2) {
+                System.out.println("Lo siento, no hay suficientes números en la fila para hacer cálculos");
             } else if (entrada.equals("+")) {
-                double suma = registros[indice - 2] + registros[indice - 1];
-                registros[indice - 2] = suma;
-                indice--;
-            } else if (entrada.equals("-") && indice < 2) {
-                System.out.println("Lo siento, no hay suficientes números en la fila para hacer calculos");
+                double num2 = registros.pop();
+                double num1 = registros.pop();
+                double suma = num1 + num2;
+                registros.push(suma);
+            } else if (entrada.equals("-") && registros.size() < 2) {
+                System.out.println("Lo siento, no hay suficientes números en la fila para hacer cálculos");
             } else if (entrada.equals("-")) {
-                double resta = registros[indice - 2] - registros[indice - 1];
-                registros[indice - 2] = resta;
-                indice--;
-            } else if (entrada.equals("/") && indice < 2) {
-                System.out.println("Lo siento, no hay suficientes números en la fila para hacer calculos");
+                double num2 = registros.pop();
+                double num1 = registros.pop();
+                double resta = num1 - num2;
+                registros.push(resta);
+            } else if (entrada.equals("/") && registros.size() < 2) {
+                System.out.println("Lo siento, no hay suficientes números en la fila para hacer cálculos");
             } else if (entrada.equals("/")) {
-                double dividir = registros[indice - 2] / registros[indice - 1];
-                registros[indice - 2] = dividir;
-                indice--;
-            } else if (entrada.equals("*") && indice < 2) {
-                System.out.println("Lo siento, no hay suficientes números en la fila para hacer calculos");
+                double num2 = registros.pop();
+                double num1 = registros.pop();
+                double dividir = num1 / num2;
+                registros.push(dividir);
+            } else if (entrada.equals("*") && registros.size() < 2) {
+                System.out.println("Lo siento, no hay suficientes números en la fila para hacer cálculos");
             } else if (entrada.equals("*")) {
-                double multiplicar = registros[indice - 2] * registros[indice - 1];
-                registros[indice - 2] = multiplicar;
-                indice--;
+                double num2 = registros.pop();
+                double num1 = registros.pop();
+                double multiplicar = num1 * num2;
+                registros.push(multiplicar);
             } else if (entrada.equals("Q")) {
                 salir = true;
             } else {
-                // Si se introduce un número, se guarda en el array y se incrementa el índice
+                // Si se introduce un número, se guarda en el stack
                 double numero = Double.parseDouble(entrada);
-                registros[indice] = numero;
-                indice++;
+                registros.push(numero);
             }
-            // Se muestran los números que hay en el array
+            // Se muestran los números que hay en el stack
             System.out.print("Registros de la fila: ");
-            for (int i = 0; i < indice; i++) {
-                System.out.print(registros[i] + " ");
+            for (double num : registros) {
+                System.out.print(num + " ");
             }
             System.out.println();
         }
