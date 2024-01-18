092760b83ff798effa17dd665ce624b3fd18a838
Alvaro <escartismx@gmail.com>
2024-01-18 16:18:33 +0100

    aer 702

--- Programacion/AceptaElReto/Ejercicio702.java
+++ Programacion/AceptaElReto/Ejercicio702.java
@@ -14,14 +14,18 @@ public class Ejercicio702 {
             int agenda = sc.nextInt();
 
             TreeMap<String, String> treemap = new TreeMap<String, String>();
-            
-
+            // Pregunto la fecha y los casos
             for (int i = 1; i <= agenda; i++) {
                 String fecha = sc.next();
-                treemap.put(fecha, sc.next());
+                String diner0 = sc.next();
+                // No me guardo la frase restante
+                sc.nextLine();
+                // Añado la fecha y los casos dentro del mapa
+                treemap.put(fecha, diner0);
                 
             }
 
+            // Recorro el mapa
             for (String key : treemap.keySet()) {
                 System.out.println("Fecha: " + key + ", Dinero: " + treemap.get(key));
             }
