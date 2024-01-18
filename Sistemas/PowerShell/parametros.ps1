a4bf601ab2f6464e92cdfbf6f5262bd8ae2f2211
Alvaro Escarti <escartismx@gmail.com>
2024-01-19 00:30:37 +0100

    parametros en powershell

---
+++ Sistemas/PowerShell/parametros.ps1
@@ -0,0 +1,17 @@
+﻿Param(
+    [string] $Primero,
+    [string] $Segundo = "valor por defecto",
+    [switch] $Tercero = $false,
+    [bool] $Cuarto,
+    [int] $Quinto
+)
+
+#a Funcionalidad:
+
+# cuando ejecutes el script debes poner como parametro: -primero hola, y 
+# "hola" será el primer parametro
+Write-Host $Primero
+Write-Host $Segundo
+Write-Host $Tercero
+Write-Host $Cuarto
+Write-Host $Quintos
