35d11493471bc05b483ef7ae2ad7720440bba750
Alvaro Escarti <escartismx@gmail.com>
2024-01-22 23:17:57 +0100

    aer 238

--- Programacion/AceptaElReto/Ejercicio238.java
+++ Programacion/AceptaElReto/Ejercicio238.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Ejercicio238 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        // 
         while (true) {
             int numBilletes = scanner.nextInt();
             int numParticipantes = scanner.nextInt();
