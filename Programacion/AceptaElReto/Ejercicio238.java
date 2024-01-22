149f4561425e5980b89e6f0ba659e0e8053b0ae4
Alvaro <escartismx@gmail.com>
2024-01-22 20:18:35 +0100

    cambios

--- Programacion/AceptaElReto/Ejercicio238.java
+++ Programacion/AceptaElReto/Ejercicio238.java
@@ -5,56 +5,23 @@ import java.util.Scanner;
 public class Ejercicio238 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        boolean finPrograma = false;
 
-        while (!finPrograma) {
+        while (true) {
             int numBilletes = scanner.nextInt();
             int numParticipantes = scanner.nextInt();
 
             if (numBilletes == 0 && numParticipantes == 0) {
-                finPrograma = true;
+                break;
             }
 
             ArrayList<Integer> billetes = new ArrayList<>();
             for (int i = 0; i < numBilletes; i++) {
-<<<<<<< HEAD
-                billetes[i] = scanner.nextInt();
-            }
-
-            // Inicializar HashMap para almacenar los billetes de cada participante
-            HashMap<Integer, ArrayList<Integer>> reparto = new HashMap<>();
-
-            // Leer la asignación de billetes a cada participante
-            for (int i = 0; i < numParticipantes; i++) {
-                int participante = scanner.nextInt();
-                reparto.put(participante, new ArrayList<>());
-
-                // Leer los billetes asignados a este participante
-                for (int j = 0; j < participante; j++) {
-                    int billeteAsignado = scanner.nextInt();
-                    reparto.get(participante).add(billeteAsignado);
-                }
-            }
-
-            // Procesar y mostrar la salida
-            for (int participante : reparto.keySet()) {
-                int totalDinero = 0;
-                System.out.print(participante + ":");
-                for (int billete : reparto.get(participante)) {
-                    totalDinero += billetes[billete - 1];
-                    System.out.print(" " + billetes[billete - 1]);
-                }
-                System.out.println(totalDinero > 0 ? "\n" + totalDinero + ":" : "\n0:");
-=======
                 billetes.add(scanner.nextInt());
->>>>>>> 97cde85d7028ba4e6ca8944dab3e107efa74b345
             }
 
             repartirBotin(numParticipantes, billetes);
             System.out.println("---");
         }
-
-        scanner.close();
     }
 
     private static void repartirBotin(int numParticipantes, ArrayList<Integer> billetes) {
