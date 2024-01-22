17d136d1c16726b06beb196ebc6037435f1566bc
Alvaro Escarti <escartismx@gmail.com>
2024-01-22 23:19:19 +0100

    aer 238

--- Programacion/AceptaElReto/Ejercicio238.java
+++ Programacion/AceptaElReto/Ejercicio238.java
@@ -1,11 +1,13 @@
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class Ejercicio238 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // 
+
         while (true) {
             int numBilletes = scanner.nextInt();
             int numParticipantes = scanner.nextInt();
@@ -25,29 +27,35 @@ public class Ejercicio238 {
     }
 
     private static void repartirBotin(int numParticipantes, ArrayList<Integer> billetes) {
-        Collections.sort(billetes, Collections.reverseOrder());
-
-        ArrayList<ArrayList<Integer>> reparto = new ArrayList<>();
+        Queue<ArrayList<Integer>> colaReparto = new LinkedList<>();
         for (int i = 0; i < numParticipantes; i++) {
-            reparto.add(new ArrayList<>());
+            colaReparto.offer(new ArrayList<>());
         }
 
-        int index = 0;
-        for (int billete : billetes) {
-            reparto.get(index).add(billete);
-            index = (index + 1) % numParticipantes;
+        for (int i = 0; i < billetes.size(); i++) {
+            colaReparto.peek().add(billetes.get(i));
+            colaReparto.offer(colaReparto.poll());
         }
-        // test
-        for (int i = 0; i < numParticipantes; i++) {
-            int total = reparto.get(i).stream().mapToInt(Integer::intValue).sum();
-            System.out.print(total + ": ");
-            for (int j = 0; j < reparto.get(i).size(); j++) {
-                System.out.print(reparto.get(i).get(j));
-                if (j < reparto.get(i).size() - 1) {
-                    System.out.print(" ");
-                }
+
+        Stack<String> resultados = new Stack<>();
+
+        while (!colaReparto.isEmpty()) {
+            ArrayList<Integer> participante = colaReparto.poll();
+            int total = 0;
+            for (int valor : participante) {
+                total += valor;
             }
-            System.out.println();
+
+            StringBuilder resultado = new StringBuilder(total + ":");
+            for (int i = 0; i < participante.size(); i++) {
+                resultado.append(" ").append(participante.get(i));
+            }
+
+            resultados.push(resultado.toString());
+        }
+
+        while (!resultados.isEmpty()) {
+            System.out.println(resultados.pop());
         }
     }
 }
