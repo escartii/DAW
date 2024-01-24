399f407b0dbe09a99eba95887c457edf80e2aa21
Álvaro <escartismx@gmail.com>
2024-01-25 00:20:21 +0100

    Matar Procesos Powershell

---
+++ Sistemas/PowerShell/process_kill.ps1
@@ -0,0 +1,13 @@
+﻿# Solicitar al usuario el ID del proceso a terminar
+$idProceso = Read-Host "Ingrese el ID del proceso a terminar"
+
+# Buscar el proceso por ID
+$proceso = Get-Process -Id $idProceso -ErrorAction SilentlyContinue
+
+if ($proceso -ne $null) {
+    # Terminar el proceso
+    Stop-Process -Id $idProceso -Force
+    Write-Host "El proceso con ID $idProceso ha sido terminado."
+} else {
+    Write-Host "No se encontró ningún proceso con el ID $idProceso en ejecución."
+}
