a3640dbf48f7e61b8d9194b8b9c53281d60397d1
Alvaro <escartismx@gmail.com>
2024-01-31 20:09:29 +0100

    Base de datos

--- BDD/ejercicio1.sql
+++ BDD/ejercicio1.sql
@@ -18,7 +18,6 @@ AND MONTH(f1.fechacreacion) = 01
 AND f1.idUsuario=33;
 
 #Ejercicio3:
-
 #Comentarios del usuario36 sobre la foto12 del usuario11
 select * from comentarios,fotos
 WHERE comentarios.idUsuario = 36
