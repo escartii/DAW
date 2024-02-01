b0cefa9fcca62a1802931bf8cea9757159158910
Alvaro Escarti <escartismx@gmail.com>
2024-02-01 18:12:13 +0100

    tiempo

--- Programacion/Programacion_Objetos/PruebaTiempo.java
+++ Programacion/Programacion_Objetos/PruebaTiempo.java
@@ -1,18 +1,68 @@
 import lib_objetos.Tiempo;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PruebaTiempo {
+
     public static void main(String[] args) {
-        Tiempo t1 = new Tiempo(10, 59, 3);
-        Tiempo t2 = new Tiempo();
+        Tiempo t1 = new Tiempo(02, 59, 3);
+        Tiempo t2 = new Tiempo(01, 59, 3);
         Tiempo t3 = new Tiempo();
+        Tiempo t4 = new Tiempo(05, 4, 3);
+        Tiempo t5 = new Tiempo(01, 4, 0);
+        Tiempo t6 = new Tiempo(02, 0, 0);
+        Tiempo t7 = new Tiempo(12, 0, 0);
+        Tiempo t8 = new Tiempo(03, 3, 0);
+        Tiempo t9 = new Tiempo(04, 0, 0);
+        Tiempo t10 = new Tiempo(00, 5, 0);
 
         System.out.println("-----------------------------------");
         System.out.print("Total sumar las horas: ");
         Tiempo.sumar(t1, t2, t3);
         System.out.println("-----------------------------------");
-        System.out.print("Total restar las horas: " );
+        System.out.print("Total restar las horas: ");
         Tiempo.restar(t1, t2, t3);
         System.out.println("-----------------------------------");
 
+        List<String> miLista = new ArrayList<String>();
+        miLista.add(t1.toString());
+        miLista.add(t2.toString());
+        miLista.add(t3.toString());
+        miLista.add(t4.toString());
+        miLista.add(t5.toString());
+        miLista.add(t6.toString());
+        miLista.add(t7.toString());
+        miLista.add(t8.toString());
+        miLista.add(t9.toString());
+        miLista.add(t10.toString());
+        // Comparamos cual es el mayor 
+        if (t1.getHoras() > t2.getHoras()) {
+            System.out.println("La hora mayor es: " + t1.toString());
+        } else {
+            System.out.println("La hora mayor es: " + t2.toString());
+        }
+        ordenarLista(miLista);
+        System.out.println("Lista ordenada: ");
+        for (String horas : miLista) {
+            System.out.println(horas);
+        }
+        System.out.println("-----------------------------------");
+    }
+
+    public static void ordenarLista(List<String> lista) {
+        int n = lista.size();
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (lista.get(j).compareTo(lista.get(j + 1)) > 0) {
+                    String temp = lista.get(j);
+                    lista.set(j, lista.get(j + 1));
+                    lista.set(j + 1, temp);
+                }
+            }
+        }
     }
 }
+
+        
+    
+
--- Programacion/Programacion_Objetos/lib_objetos/Tiempo.java
+++ Programacion/Programacion_Objetos/lib_objetos/Tiempo.java
@@ -1,43 +1,50 @@
 package lib_objetos;
 
-public class Tiempo {
+public class Tiempo implements Comparable<Tiempo> {
     private int horas;
     private int minutos;
     private int segundos;
 
     // Constructor por defecto
-    public Tiempo (){
-        this.horas = 10;
-        this.minutos = 10;
-        this.segundos = 10;
+    public Tiempo() {
+        this.horas = 0;
+        this.minutos = 0;
+        this.segundos = 0;
     }
+
     // Constructor con parametros
     public Tiempo(int horas, int minutos, int segundos) {
         this.horas = horas;
         this.minutos = minutos;
         this.segundos = segundos;
     }
-    
+
     // Getters y Setters
     public int getHoras() {
         return horas;
     }
+
     public void setHoras(int horas) {
         this.horas = horas;
     }
+
     public int getMinutos() {
         return minutos;
     }
+
     public void setMinutos(int minutos) {
         this.minutos = minutos;
     }
+
     public int getSegundos() {
         return segundos;
     }
+
     public void setSegundos(int segundos) {
         this.segundos = segundos;
     }
-    // Metodo que suma 
+
+    // Metodo que suma
     public static void sumar(Tiempo t1, Tiempo t2, Tiempo t3) {
         t3.setHoras(t1.getHoras() + t2.getHoras());
         t3.setMinutos(t1.getMinutos() + t2.getMinutos());
@@ -54,7 +61,8 @@ public class Tiempo {
 
         System.out.println(t3.toString());
     }
-    // Metodo que resta 
+
+    // Metodo que resta
     public static void restar(Tiempo t1, Tiempo t2, Tiempo t3) {
         t3.setHoras(t1.getHoras() - t2.getHoras());
         t3.setMinutos(t1.getMinutos() - t2.getMinutos());
@@ -71,11 +79,31 @@ public class Tiempo {
 
         System.out.println(t3.toString());
     }
+
     // Metodo toString para formatear la salida
     public String toString() {
-        return horas + "h " + minutos + "m " + segundos + "s";
+        String horaString = (horas < 10) ? "0" + horas : String.valueOf(horas);
+        String minutosString = (minutos < 10) ? "0" + minutos : String.valueOf(minutos);
+        String segundosString = (segundos < 10) ? "0" + segundos : String.valueOf(segundos);
+        return horaString + "h " + minutosString + "m " + segundosString + "s";
     }
-    
+
+    private int getTotalSeconds() {
+        return horas * 3600 + minutos * 60 + segundos;
+    }
+
+    // Implementacion del metodo compareTo
+    @Override
+    public int compareTo(Tiempo other) {
+        int totalSeconds1 = this.getTotalSeconds();
+        int totalSeconds2 = other.getTotalSeconds();
+
+        if (totalSeconds1 < totalSeconds2) {
+            return -1;
+        } else if (totalSeconds1 > totalSeconds2) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
 }
-
-
