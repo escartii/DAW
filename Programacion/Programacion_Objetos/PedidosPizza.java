3893477609763ddba387b84d963aa2899f6e0699
Alvaro Escarti <escartismx@gmail.com>
2024-02-02 15:24:02 +0100

    Pizza

---
+++ Programacion/Programacion_Objetos/PedidosPizza.java
@@ -0,0 +1,16 @@
+import lib_objetos.Pizza;
+
+public class PedidosPizza {
+    public static void main(String[] args) {
+        Pizza p1 = new Pizza("mediana", "margarita");
+        Pizza p2 = new Pizza("familiar", "funghi");
+        p2.sirve();
+        Pizza p3 = new Pizza("mediana", "cuatro quesos");
+        System.out.println(p1);
+        System.out.println(p2);
+        System.out.println(p3);
+        p2.sirve();
+        System.out.println("pedidas: " + Pizza.getTotalPedidas());
+        System.out.println("servidas: " + Pizza.getTotalServidas());
+    }
+}
---
+++ Programacion/Programacion_Objetos/lib_objetos/Pizza.java
@@ -0,0 +1,72 @@
+package lib_objetos;
+
+public class Pizza {
+    // Atributos
+    private String tamaño;
+    private String tipo;
+    private String estado;
+    private static int totalPedidas = 0;
+    private static int totalServidas = 0;
+
+    // Constructor
+    public Pizza(String tamaño, String tipo) {
+        this.tamaño = tamaño;
+        this.tipo = tipo;
+        this.estado = "pedida";
+        totalPedidas++;
+    }
+
+    // Métodos
+    public void sirve() {
+        if (estado.equals("pedida")) {
+            estado = "servida";
+            totalServidas++;
+        } else {
+            System.out.println("Esta pizza ya ha sido servida.");
+        }
+    }
+
+    public String toString() {
+        return "Pizza: tamaño=" + tamaño + ", tipo=" + tipo + ", estado=" + estado;
+    }
+    // Getters y Setters
+    public String getTamaño() {
+        return tamaño;
+    }
+
+    public void setTamaño(String tamaño) {
+        this.tamaño = tamaño;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public static void setTotalPedidas(int totalPedidas) {
+        Pizza.totalPedidas = totalPedidas;
+    }
+
+    public static void setTotalServidas(int totalServidas) {
+        Pizza.totalServidas = totalServidas;
+    }
+
+    public static int getTotalPedidas() {
+        return totalPedidas;
+    }
+
+    public static int getTotalServidas() {
+        return totalServidas;
+    }
+}
