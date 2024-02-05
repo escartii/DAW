37e90630af8a082cc220d0a6290537be02a90849
Álvaro <escartismx@gmail.com>
2024-02-05 16:44:31 +0100

    Copia de seguridad TOTAL

---
+++ Sistemas/PowerShell/Copiatotal.ps1
@@ -0,0 +1,26 @@
+﻿$ruta = "C:\Users\Administrador\Desktop\Apuntes\*"
+
+# Obtener la fecha en el formato deseado (AAAA-MM-DD)
+$fecha = Get-Date -Format "yyyy-MM-dd"
+
+# Nombre del archivo ZIP con la fecha en el formato especificado
+$nombreArchivoZip = "$fecha-CopiaSeguridad.zip"
+
+# Ruta de destino para la copia de seguridad
+$rutaBackup = "F:\BACKUP\"
+
+# Ruta completa del archivo ZIP
+$rutaArchivoZip = Join-Path -Path $rutaBackup -ChildPath $nombreArchivoZip
+
+# Comprimir solo los archivos modificados en un archivo ZIP
+Compress-Archive -Path $ruta -DestinationPath $rutaArchivoZip -Force
+#Borro del Escritorio
+Remove-Item "C:\Users\Administrador\Desktop\Apuntes\*"
+
+#Descomprimir
+Expand-Archive -Path $rutaBackup$nombreArchivoZip -DestinationPath "C:\Users\Administrador\Desktop\Apuntes"
+Write-Host "Se ha copiado correctamente"
+
+
+
+
