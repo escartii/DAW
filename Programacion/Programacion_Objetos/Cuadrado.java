8aea2545d42d72f822a9602faaded6e030802663
Alvaro Escarti <escartismx@gmail.com>
2024-02-08 15:45:30 +0100

    Ejercicio abstracto con figuras

--- Programacion/Programacion_Objetos/Cuadrado.java
+++ Programacion/Programacion_Objetos/Cuadrado.java
@@ -1,20 +1,21 @@
+// Definición de la clase Cuadrado que extiende de la clase Figura
 public class Cuadrado extends Figura {
-    private int lado;
+    private int lado; // Variable para almacenar el valor del lado del cuadrado
 
+    // Constructor de la clase Cuadrado que recibe el valor del lado como parámetro
     public Cuadrado(int lado) {
         this.lado = lado;
     }
 
+    // Implementación del método area() de la clase Figura
     @Override
     public double area() {
-        return lado * lado;
+        return lado * lado; // Cálculo del área del cuadrado (lado * lado)
     }
 
+    // Implementación del método perimetro() de la clase Figura
     @Override
     public double perimetro() {
-        return 4 * lado;
+        return 4 * lado; // Cálculo del perímetro del cuadrado (4 * lado)
     }
-
 }
-
-
--- Programacion/Programacion_Objetos/Triangulo.java
+++ Programacion/Programacion_Objetos/Triangulo.java
@@ -1,17 +1,24 @@
+// Clase Triangulo que hereda de la clase Figura
 public class Triangulo extends Figura {
-    private int base;
-    private int altura;
+    private int base; // Variable para almacenar la base del triángulo
+    private int altura; // Variable para almacenar la altura del triángulo
+    
+    // Constructor de la clase Triangulo que recibe la base y la altura como parámetros
     public Triangulo(int base, int altura) {
-        this.base = base;
-        this.altura = altura;
+        this.base = base; // Asigna el valor de la base recibida al atributo base de la clase
+        this.altura = altura; // Asigna el valor de la altura recibida al atributo altura de la clase
     }
+    
+    // Método para calcular el área del triángulo
     @Override
     public double area() {
-        return (base * altura) / 2;
+        return (base * altura) / 2; // Retorna el resultado del cálculo del área del triángulo
     }
+    
+    // Método para calcular el perímetro del triángulo
     @Override
     public double perimetro() {
-        return base + altura + Math.sqrt((base * base) + (altura * altura));
+        return base + altura + Math.sqrt((base * base) + (altura * altura)); // Retorna el resultado del cálculo del perímetro del triángulo
     }   
     
 }
