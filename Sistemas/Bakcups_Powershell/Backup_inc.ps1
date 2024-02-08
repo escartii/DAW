8f6bea4da79f98a5a10b9141f1d0f4de45a10837
Alvaro <escartismx@gmail.com>
2024-02-08 19:03:13 +0100

    scripts powershell

---
+++ Sistemas/Bakcups_Powershell/Backup_inc.ps1
@@ -0,0 +1,47 @@
+﻿# Ruta de origen
+$sourcePath = "C:\Users\Administrador\Desktop\APUNTES"
+
+# Ruta de destino para el backup
+$backupPath = "F:\BACKUP"
+
+# Obtener la fecha actual
+$currentDate = Get-Date -Format "yyyy-MM-dd"
+
+# Crear la carpeta de respaldo con la fecha actual si no existe
+$backupFolder = Join-Path -Path $backupPath -ChildPath $currentDate
+New-Item -ItemType Directory -Path $backupFolder -Force
+#fecha del ultimo backup
+$lastBackupDate = Get-ChildItem $backupPath | Where-Object { $_.PSIsContainer } | Sort-Object LastWriteTime -Descending | Select-Object -First 1
+if ($lastBackupDate -eq $null) {
+    # Si es la primera copia, copiar todos los archivos
+    Copy-Item -Path "$sourcePath\*" -Destination $backupFolder -Recurse -Force
+} else {
+
+    # Copiar solo los archivos modificados desde la última copia
+    $lastBackupDate = $lastBackupDate.Name
+    $filesToCopy = Get-ChildItem -Path $sourcePath | Where-Object { $_.LastWriteTime -gt $lastBackupDate }
+    Copy-Item -Path $filesToCopy.FullName -Destination $backupFolder -Recurse -Force
+
+
+}
+
+# Comprimir la carpeta de respaldo en un archivo zip
+$zipFileName = "$currentDate-docs_inc.zip"
+$zipFilePath = Join-Path -Path $backupPath -ChildPath $zipFileName
+Compress-Archive -Path $backupFolder\* -DestinationPath $zipFilePath -Force
+
+# Verificar si el archivo zip no existe antes de crearlo
+if (-not (Test-Path $zipFilePath)) {
+    # Comprimir la carpeta de respaldo en un archivo zip
+    Compress-Archive -Path $backupFolder\* -DestinationPath $zipFilePath -Force
+    Write-Host "Archivo de copia de seguridad creado en: $zipFilePath"
+} else {
+    Write-Host "El archivo de copia de seguridad ya existe en la ruta: $zipFilePath. No se ha creado otro archivo."
+}
+
+
+# Eliminar los archivos originales, excluyendo el archivo zip
+Remove-Item -Recurse -Exclude *.zip -Path F:\BACKUP\
+
+Write-Host "Copia de seguridad incremental completada. Archivo creado: $zipFilePath"
+
---
+++ Sistemas/Bakcups_Powershell/copia_docs_inc.ps1
@@ -0,0 +1,45 @@
+﻿# Nombre del script: copia_docs_inc.ps1
+
+# Variables para las rutas de origen y destino
+$source = "$HOME\Desktop\APUNTES2"
+$destination = "C:\BACKUP\"
+
+# Obtener la fecha actual en el formato deseado para poder construir el nombre del fichero
+$date = Get-Date -Format "yyyy-MM-dd"
+
+# Crear el nombre del archivo zip (nombre y ruta: ejemplo C:\BACKUP\2024-02-08-docs_inc.zip)
+$zipfile = $destination + $date + "-docs_inc.zip"
+
+# Obtener la fecha de ayer
+$lastDay = (Get-Date).AddDays(-1)
+
+# Obtener los archivos modificados desde la fecha de ayer
+$lastDayFiles = Get-ChildItem -Path $source -Recurse | Where-Object { $_.LastWriteTime -gt  $lastDay }
+
+# Comprobar si existen ficheros modificados en el último día y copiarlos a una carpeta temporal
+if ($lastDayFiles -ne $null) {
+    # Crear una carpeta temporal para almacenar los archivos modificados con su estructura de directorios
+    ### $tempFolder = Join-Path -Path $destination -ChildPath "temp"   --> otra forma de concatenar 2 path
+    $tempFolder = $destination + "temp"
+    # Crea una carpeta temporal, no importa que ya existiera porque es algo temporal
+    New-Item -ItemType Directory -Force -Path $tempFolder
+
+    # Copiar los archivos que han sido modificados a la carpeta temporal
+    foreach ($file in $lastDayFiles) {
+        $destinationFile = $file.FullName.Replace($source, $tempFolder)
+        # Creo los directorios necesarios
+        New-Item -ItemType Directory -Force -Path (Split-Path -Path $destinationFile -Parent)
+        # copio el fichero
+        Copy-Item -Path $file.FullName -Destination $destinationFile
+    }
+
+    # Creamos el archivo comprimido .zip con los ficheros modificados
+    Compress-Archive -Path (Join-Path -Path $tempFolder -ChildPath "*") -DestinationPath $zipfile
+    
+    # Eliminamos la carpeta temporal
+    Remove-Item -Path $tempFolder -Recurse -Force
+
+    Write-Host "Copia de seguridad incremental creada en $zipfile"
+} else {
+    Write-Host "No hay archivos modificados en el último día."
+}
