87b8e21a87f9405056f2d4d4405b82bf38f273ae
Alvaro <escartismx@gmail.com>
2024-02-12 14:54:58 +0100

    añadir tabla fotos con mysql

--- BDD/Random.sql
+++ BDD/Random.sql
@@ -1,3 +1,8 @@
+# Crea la columna KB en la tabla fotos
+ALTER TABLE `instagram_low_cost`.`fotos` 
+ADD COLUMN `KB` INT NOT NULL AFTER `fechaCreacion`;
+
+
 # Le da un valor aleatorio a la columna KB de la tabla fotos
 UPDATE `instagram_low_cost`.`fotos`
 SET KB = FLOOR(128 + RAND() * (999 - 128))
