8d5ee69ff974f3f929e620d37c65bca97234baee
Alvaro Escarti <escartismx@gmail.com>
2024-03-14 22:50:24 +0100

    Serializar y deserializar ficheros en java

--- Programacion/Ficheros/Interfaz.java
+++ Programacion/Ficheros/Interfaz.java
@@ -2,15 +2,24 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Interfaz {
+    // Creo un metodo para limpiar la terminal :)
+    public static void limpiarPantalla() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+
     public static void main(String[] args) {
+        limpiarPantalla();
+        Scanner sc = new Scanner(System.in);
         ArrayList<Galaxias> objetos = new ArrayList<>();
-        
+
         try {
             ObjectInputStream ois = new ObjectInputStream(new FileInputStream("SAC.bin"));
-            
+
             while (true) {
                 // Deserializar el fichero
                 try {
@@ -22,64 +31,83 @@ public class Interfaz {
                     break;
                 }
             }
+
             ois.close();
         } catch (IOException e) {
             System.out.println("Error al leer el archivo: " + e.getMessage());
             System.exit(1);
         }
 
-        Scanner sc = new Scanner(System.in);
-        int opcion;
+        int opcion = 0;
         do {
-            System.out.println("\nMenú:");
-            System.out.println("1. Nombre del objeto");
-            System.out.println("2. Constelación");
-            System.out.println("3. Magnitud límite");
-            System.out.println("4. Salir");
-            System.out.print("Seleccione una opción: ");
-            opcion = sc.nextInt();
-            switch (opcion) {
-                case 1:
-                    System.out.print("Introduzca el nombre del objeto: ");
-                    String nombre = sc.next();
-                    int i = 0;
-                    while (i < objetos.size()) {
-                        Galaxias galaxia = objetos.get(i);
-                        if (galaxia.getObject().equalsIgnoreCase(nombre)) {
-                            System.out.println(galaxia.getObject());
+            try {
+                System.out.println(" ");
+                System.out.println("***** MENU ******:");
+                System.out.println("1. Nombre del objeto");
+                System.out.println("2. Constelación");
+                System.out.println("3. Magnitud límite");
+                System.out.println("4. Salir");
+                System.out.print("Seleccione una opción: ");
+                opcion = sc.nextInt();
+
+                switch (opcion) {
+                    case 1:
+                        System.out.print("Introduzca el nombre del objeto: ");
+                        String nombre = sc.next();
+                        int i = 0;
+                        while (i < objetos.size()) {
+                            Galaxias galaxia = objetos.get(i);
+                            if (galaxia.getObject().equalsIgnoreCase(nombre)) {
+                                System.out.println(galaxia.getObject());
+                            }
+                            i++;
                         }
-                        i++;
-                    }
-                    break;
-                case 2:
-                    System.out.print("Introduce la constelacion: ");
-                    String constelacion = sc.next();
-                    for (Galaxias galaxia : objetos) {
-                        // por si acaso introduce en minusculas
-                        if (galaxia.getCON().equalsIgnoreCase(constelacion)) {
-                            System.out.println(galaxia.getCON()
-                             + " " + galaxia.getRA()
-                             + " " + galaxia.getDEC()
-                             + " " + galaxia.getDEC()
-                             );
+                        break;
+
+                    case 2:
+                        System.out.print("Introduce la constelacion: ");
+                        String constelacion = sc.next();
+                        for (Galaxias galaxia : objetos) {
+                            // por si acaso introduce en minusculas
+                            if (galaxia.getCON().equalsIgnoreCase(constelacion)) {
+                                System.out.println(galaxia.getCON()
+                                        + " " + galaxia.getRA()
+                                        + " " + galaxia.getDEC()
+                                        + " " + galaxia.getDEC());
+                            }
                         }
-                    }
-                    break;
-                case 3:
-                    System.out.print("Introduce la magnitud: ");
-                    double magni = sc.nextDouble();
-                    for (Galaxias galaxia : objetos) {
-                        if (Double.parseDouble(galaxia.getMAG()) <= magni) {
-                            System.out.println(galaxia.getMAG());
+                        break;
+
+                    case 3:
+                        System.out.print("Introduce la magnitud: ");
+                        double magni = sc.nextDouble();
+                        for (Galaxias galaxia : objetos) {
+                            // Filtro la busquea para que sea menor o igual que lo que ha introducido el
+                            // usuario
+                            if (Double.parseDouble(galaxia.getMAG()) <= magni) {
+                                System.out.println(galaxia.getObject()
+                                        + " " + galaxia.getCON()
+                                        + " " + galaxia.getRA()
+                                        + " " + galaxia.getDEC()
+                                        + " " + galaxia.getMAG());
+                            }
                         }
-                    }
-                    break;
-                case 4:
-                    System.out.println("FIN DEL PROGRAMA");
-                    System.exit(0);
-                default:
-                    System.out.println("Introduce un numero valido.");
+                        break;
+
+                    case 4:
+                        System.out.println("FIN DEL PROGRAMA");
+                        System.exit(0);
+
+                    default:
+                        System.out.println("Introduce un numero valido.");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Caracter no valido(seguramente has introducido un punto ejemplo 13.8): ");
+                opcion = 4;
+            } catch (Exception e) {
+                System.out.println("Error: " + e.getMessage());
             }
+
         } while (opcion != 4);
         sc.close();
     }
