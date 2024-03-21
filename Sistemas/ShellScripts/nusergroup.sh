ca48f38c27f8eb3b3399d3747600ff7d3254ffe8
Alvaro <escartismx@gmail.com>
2024-03-21 19:33:15 +0100

    columna 4 passwd

--- Sistemas/ShellScripts/nusergroup.sh
+++ Sistemas/ShellScripts/nusergroup.sh
@@ -4,4 +4,6 @@
 
 #uniq sirve para quitar las lineas repetidas
 
+#Cut es un comando que se utiliza para cortar una parte de un archivo llamado delimitador
+
 cut -d ':' -f4 /etc/passwd | sort -n | uniq -c | sort -nr
