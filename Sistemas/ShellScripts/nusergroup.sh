d005cfab7f5c27c6b27a76c3bb12f8b47ccd9491
Alvaro <escartismx@gmail.com>
2024-03-21 19:51:11 +0100

    ejercicios shellscript

--- Sistemas/ShellScripts/nusergroup.sh
+++ Sistemas/ShellScripts/nusergroup.sh
@@ -5,5 +5,6 @@
 #uniq sirve para quitar las lineas repetidas
 
 #Cut es un comando que se utiliza para cortar una parte de un archivo llamado delimitador
-
+#esto es un ejemplo con el sort y el cut
+sort -t -k 3 /etc/passwd 
 cut -d ':' -f4 /etc/passwd | sort -n | uniq -c | sort -nr
