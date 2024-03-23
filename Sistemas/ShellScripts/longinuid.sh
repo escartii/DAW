ccd7149d3f27a3a1d0602dbc1cc8892e89e4434a
Alvaro Escarti <escartismx@gmail.com>
2024-03-23 22:04:39 +0100

    wik

--- Sistemas/ShellScripts/longinuid.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#!/bin/bash
-read -p "Archivo: " archivo
-#output-delimitir, cambia el delimitador de salida, (el de entrada es ":", y ahora el de salida es ";")
-cat /etc/passwd | cut -d ':' -f1,2 --output-delimiter=";" | sort -t ';' -k2 -n > $archivo
--- Sistemas/ShellScripts/ordenacol.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-#Alvaro Escarti :))
-# esto ordena el archivo usuarios.csv por el campo 2 y muestra las 10 primeras lineas
-sort -t ',' -k2 usuarios.csv | head -n 10
