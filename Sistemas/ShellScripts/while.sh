1bcfe00fac5071e6f6df163d559c8fe8a4993c49
escartii <escartii@escartii.alvaro>
2024-03-25 21:16:43 +0100

    ejemplos de bucles while

--- Sistemas/ShellScripts/while.sh
+++ Sistemas/ShellScripts/while.sh
@@ -4,3 +4,26 @@ while true; do
     date >> date.log
     sleep 1;
 done
+
+
+#contador utilizando un bucle while
+contador=1
+while [ $contador -le 5 ]; do
+
+	echo "COntador: $contador"
+	contador=$((contador+1))
+done
+
+archivo = "ejemplo.txt"
+while IFS=read -r linea
+do
+	echo "linea leida: $inea"
+done < "$archivo"
+
+entrada =""
+while [ "$entrada" != "salir" ]
+do
+	echo "Ingresa 'salir' para finalizar"
+	read entrada
+	echo "erntrada :$entrada"
+done
