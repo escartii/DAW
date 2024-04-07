089edc1615433b74d8b0f2f197dc2425d1f48f6d
Alvaro Escarti <escartismx@gmail.com>
2024-04-07 20:38:57 +0200

    update

--- Programacion/AccesoDatos/Main.java
+++ Programacion/AccesoDatos/Main.java
@@ -3,7 +3,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.ResultSet;
-
+// test
 public class Main {
     public static void main(String[] args) {
 
