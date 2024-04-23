c36a6b1c1e8f83c92a76dd1d4786a2ef04abc8fe
alvaro escarti <escartismx@gmail.com>
2024-04-23 16:10:23 +0200

    copias con tar

--- Sistemas/ShellScripts/Boletin-TAR/ejercicio1.sh
+++ Sistemas/ShellScripts/Boletin-TAR/ejercicio1.sh
@@ -2,15 +2,28 @@
 
 #Alvaro Escarti
 
+#if [ "$(whoami)" != "root" ]; then
+#    echo "Este script necesita privilegios de root para ejecutarse."
+#    exit 1
+#fi
 
-if [ "$(whoami)" != "root" ]; then
-    echo "Este script necesita privilegios de root para ejecutarse."
+#read -p "introduce tu nombre de usuario: " usuario
+#read -p "introduce tu contraseña: " password
+
+#useradd -m -p $(openssl passwd -1 $password) $usuario
+
+#echo "Usuario creado correctamente"
+
+#Año, mes y dia
+current_date=$(date +"%Y-%m-%d")
+#Hora y minutos
+current_time=$(date +"%H-%M")
+
+if [ -d /media/escartii ]; then
+    echo "la particion está montada"
+    tar -czf /media/escartii/$current_date-$current_time.tar.gz /home/carles/Escritorio
+else
+    echo "la particion no esta montada"
     exit 1
 fi
-
-read -p "introduce tu nombre de usuario: " usuario
-read -p "introduce tu contraseña: " password
-
-useradd -m -p $(openssl passwd -1 $password) $usuario
-
 
