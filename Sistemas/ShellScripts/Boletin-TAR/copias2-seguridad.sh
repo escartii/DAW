0900afb1545c4035e223492df1f2031b5cd0be9a
escartii <escartismx@gmail.com>
2024-04-24 23:46:16 +0200

    ejercicios bash

--- Sistemas/ShellScripts/Boletin-TAR/copias2-seguridad.sh
+++ Sistemas/ShellScripts/Boletin-TAR/copias2-seguridad.sh
@@ -10,23 +10,21 @@ fi
 if mount | grep -q "/dev/sdb4" && [ -d /BACKUPS ]; then
     tar cv -g /BACKUPS/lista-incremental.file -f /BACKUPS/noticias-$(date +%d-%m-%Y).tar /NOTICIAS
 else 
-    echo "La partición /dev/sdb4 no está montada."
-    echo "USO: sudo mount /dev/sdb4 /BACKUPS"
-    read -p "¿Desea montar la partición /dev/sdb4 en /BACKUPS? (s/n): " respuesta
-    if [ $respuesta = "s" ] || [ $respuesta = "si" ] || [ $respuesta = "y" ] || [ $respuesta == "yes" ]; then
-        sudo mount /dev/sdb4 /BACKUPS
-        echo "Partición montada..."
-        if [ ! -d /BACKUPS ]; then
-            echo "La ruta o el fichero no existe..."
-            echo -e "\e[31mSaliendo del programa\e[0m"
-            mkdir /BACKUPS
-            tar cv -g /BACKUPS/lista-incremental.file -f /BACKUPS/noticias-$(date +%d-%m-%Y).tar /NOTICIAS
-        else 
-            tar cv -g /BACKUPS/lista-incremental.file -f /BACKUPS/noticias-$(date +%d-%m-%Y).tar /NOTICIAS
-        fi
+    if [ ! -d /BACKUPS ]; then
+        echo "La ruta no existe"
+        sudo mkdir /BACKUPS
     else
-        echo "Saliendo del programa..."
-        exit 1
+        echo "-/dev/sdb4 no está montada."
+        echo "USO: sudo mount /dev/sdb4 /BACKUPS"
+        read -p "¿Desea montar la partición /dev/sdb4 en /BACKUPS? (s/n): " respuesta
+        if [ $respuesta = "s"  || $respuesta = "si" || $respuesta = "y" | $respuesta == "yes"]; then
+            sudo mount /dev/sdb4 /BACKUPS
+            echo "Partición montada..."
+            tar cv -g /BACKUPS/lista-incremental.file -f /BACKUPS/noticias-$(date +%d-%m-%Y).tar /NOTICIAS
+        else
+            echo "Saliendo del programa..."
+            exit 1
+        fi
     fi
 fi
 
